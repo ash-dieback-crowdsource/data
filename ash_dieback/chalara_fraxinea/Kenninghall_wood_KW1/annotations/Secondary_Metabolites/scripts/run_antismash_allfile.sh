4e9f869e01d0eae3a1a36665e9526cb59b103c15
Jason Stajich <jason@bioperl.org>
2016-04-26 13:16:13 -0700

    simplify

--- ash_dieback/chalara_fraxinea/Kenninghall_wood_KW1/annotations/Secondary_Metabolites/scripts/run_antismash_allfile.sh
+++ ash_dieback/chalara_fraxinea/Kenninghall_wood_KW1/annotations/Secondary_Metabolites/scripts/run_antismash_allfile.sh
@@ -2,10 +2,10 @@
 CPUS=12
 
 module load antismash/2.0.2
-INFILE=chalara_fraxinea.KW1.embl
-ODIR=antismash_out
+INFILE=make_embl/embl_ready_fixed/*.embl
+ODIR=antismash_all_out
 DIR=/bigdata/jstajich/ash-dieback/data/ash_dieback/chalara_fraxinea/Kenninghall_wood_KW1/annotations/Secondary_Metabolites
 mkdir $DIR/$ODIR;
 cd $DIR/$ODIR
 echo $DIR/$NAME
-run_antismash --eukaryotic --input-type nucl  --clusterblast --subclusterblast  --inclusive --smcogs --full-hmmer --full-blast --enable-xls --cpus $CPUS $DIR/$INFILE
+run_antismash --eukaryotic --input-type nucl  --clusterblast --subclusterblast  --inclusive --smcogs --full-hmmer --full-blast --outputfolder $DIR/$ODIR --cpus $CPUS $DIR/$INFILE
